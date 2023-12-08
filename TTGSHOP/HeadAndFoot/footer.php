95c300da0f6ce329753754fcc8186cf544adcdf3
Dattoon <ttmdvhd@gmail.com>
2023-12-08 16:49:30 +0700

    Dong bo

--- TTGSHOP/HeadAndFoot/footer.php
+++ TTGSHOP/HeadAndFoot/footer.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./TTGSHOP/fonts/themify-icons/themify-icons.css">
-    <link rel="stylesheet" href="./TTGSHOP/style.css">
-    <link rel="stylesheet" href="./HFstyle.css">
-    
-    <title>Document</title>
-</head>
-    
-    
-    
-    
-    
-    
-    
+
+<body> 
+  
     <!--Beging footer -->   
      <div id="footer">
         <div class="footer-top">
