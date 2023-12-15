62051e772ce7eb0dc8f6e6305afdbaf9567c5797
Dattoon <ttmdvhd@gmail.com>
2023-12-15 22:56:44 +0700

    fix

--- DDGSHOP/DDG.php
+++ DDGSHOP/DDG.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Icon-->
-    <link rel="stylesheet" href="./DDGSHOP/fonts/themify-icons/themify-icons.css">
-    <link rel="stylesheet" href="./DDGSHOP/fonts/fontawesome/all.css">
+    <link rel="stylesheet" href="./DDGSHOP/view/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="./DDGSHOP/view/fonts/fontawesome/all.css">
     <!--CSS-->
-    <link rel="stylesheet" href="./DDGSHOP/CSS/bootstrap.min.css">
-    <link rel="stylesheet" href="./DDGSHOP/CSS/style-themes.scss.css">
+    <link rel="stylesheet" href="./DDGSHOP/view/CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./DDGSHOP/view/CSS/style-themes.scss.css">
     <!--JS-->
-    <script src="./DDGSHOP/JS/jquery-3.5.1.min.js"></script>
-    <script src="./DDGSHOP/JS/scripts.js"></script>
-    <script src="./DDGSHOP/JS/LinhTinhJS/api.jquery.js"></script>
-    <script src="./DDGSHOP/JS/LinhTinhJS/beacon.min.js"></script>
-    <script src="./DDGSHOP/JS/LinhTinhJS/cast_sender.js"></script>
+    <script src="./DDGSHOP/view/JS/jquery-3.5.1.min.js"></script>
+    <script src="./DDGSHOP/view/JS/scripts.js"></script>
+    <script src="./DDGSHOP/view/JS/LinhTinhJS/api.jquery.js"></script>
+    <script src="./DDGSHOP/view/JS/LinhTinhJS/beacon.min.js"></script>
+    <script src="./DDGSHOP/view/JS/LinhTinhJS/cast_sender.js"></script>
 
     <title>Document</title>
 </head>
@@ -24,7 +24,7 @@
 
 
 
-    <?php include("./view/header.php"); 
+    <?php include "./view/header.php"; 
 
          switch ($act){
                 case "":
@@ -38,10 +38,10 @@
     
 
    
-    include("./view/main.php"); 
+    include "./view/main.php"; 
    
     
-    include("./view/footer.php"); ?>
+    include "./view/footer.php"; ?>
 
 
 </body>
