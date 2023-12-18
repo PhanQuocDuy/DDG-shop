4ed6206d3c8c271a9ec7eda5f46aff3646b7e853
PhanQuocDuy <quocduy0939@gmail.com>
2023-12-18 14:52:50 +0700

    xong

--- DDGSHOP/JS/button.js
+++ DDGSHOP/JS/button.js
@@ -4,7 +4,7 @@ btn.forEach(function (button, index) {
         {
             var parentElement = this.closest('.product-loop');
             var productIMG = parentElement.querySelector('img').src
-            var productName = parentElement.querySelector('H3').innerText
+            var productName = parentElement.querySelector('h3').innerText
             var productPrice = parentElement.querySelector('span').innerText
             addcart(productIMG,productName,productPrice);
         }
@@ -14,12 +14,21 @@ btn.forEach(function (button, index) {
 
 function addcart(productIMG,productName,productPrice){
     var addtr = document.createElement("tr")
-    var trcontent = '<tr style="font-family: Arial, sans-serif; font-size: 14px;"><td style="display: flex; align-items: center"><img src="'+productIMG+'" style="width: 25%; height: auto;" alt=" "><h7>'+productName+'</h7></td><td style="padding: 10px;"><span style="font-weight: bold; color: black;">'+productPrice+'</span></td><td style="padding: 10px; background-color: #f8f9fa; text-align: center; border: 1px solid #ced4da; border-radius: 4px;"><input type="number" value="1" min="1" style="width: 30px; outline: none;" /></td><td style="cursor: pointer; padding: 10px; background-color: #f8f9fa; text-align: center; border: 1px solid #ced4da; border-radius: 4px;">Xóa</td></tr>'
+    var cartItem = document.querySelectorAll(".table-cart-view tr")
+    for (var i=0; i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".nameItem")
+        if(productT[i].innerHTML==productName ){
+            alert("Sản phẩm của bạn đã có trong giỏ hàng")
+            return
+        }
+    }
+    var trcontent = '<tr style="font-family: Arial, sans-serif; font-size: 14px;"><td style="display: flex; align-items: center"><img src="'+productIMG+'" style="width: 25%; height: auto;" alt=" "><h7 class="nameItem">'+productName+'</h7></td><td style="padding: 10px;"><span style="font-weight: bold; color: black;">'+productPrice+'</span></td><td style="padding: 10px; background-color: #f8f9fa; text-align: center; border: 1px solid #ced4da; border-radius: 4px;"><input type="number" value="1" min="1" style="width: 30px; outline: none;" /></td><td style="cursor: pointer; padding: 10px; background-color: #f8f9fa; text-align: center; border: 1px solid #ced4da; border-radius: 4px;"><span class="deleteItem">Xóa</span></td></tr>'
     addtr.innerHTML = trcontent
     var cartTable = document.querySelector('table.table-cart-view')
     // console.log(cartTable)
     cartTable.append(addtr)
     carttotal()
+    deletecart()
 }
 function carttotal(){
     var cartItem = document.querySelectorAll(".table-cart-view tr")
@@ -32,8 +41,31 @@ function carttotal(){
         totalA = inputValue * productPrice
         totalC = totalC + totalA
         // Chuyển đổi lại thành dạng có dấu phẩy
-        totalD = totalC.toLocaleString('de-DE')
+        // totalD = totalC.toLocaleString('de-DE')
     }
     var cartTotalA = document.querySelector(".total-view-cart span")
-    cartTotalA.innerHTML = totalD
+    cartTotalA.innerHTML = totalC.toLocaleString('de-DE')
+    inputchane()
+}
+//--------Delete cart ----------//
+function deletecart(){
+    var cartItem = document.querySelectorAll(".table-cart-view tr")
+    for (var i=0; i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".deleteItem")
+        productT[i].addEventListener("click",function(event){
+            var cartDelete = event.target
+            var cartitemR = cartDelete.parentElement.parentElement
+            cartitemR.remove()
+            carttotal()
+        })
+    }
+}
+function inputchane(){
+    var cartItem = document.querySelectorAll(".table-cart-view tr")
+    for (var i=0; i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input")
+        inputValue.addEventListener("change",function(){
+            carttotal()
+        })
+    }
 }
